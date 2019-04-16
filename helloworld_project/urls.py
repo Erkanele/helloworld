7260d0b96ae5702cee7d24bbbbcd8d69293a5383
erkanmanubles <erik.elebro@gmail.com>
2019-04-16 19:38:43 +0200

    testing

--- helloworld_project/urls.py
+++ helloworld_project/urls.py
@@ -1,4 +1,3 @@
-"""helloworld_project URL Configuration
 from django.contrib import admin
 from django.urls import path, include
 urlpatterns = [
--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-
 from django.urls import path
 from . import views
 urlpatterns = [
