b6ff80b9a64ced2c8508630b41c5eee501e866e7
erkanmanubles <erik.elebro@gmail.com>
2019-04-16 19:34:12 +0200

    comment

---
+++ helloworld_project/urls.py
@@ -0,0 +1,7 @@
+"""helloworld_project URL Configuration
+from django.contrib import admin
+from django.urls import path, include
+urlpatterns = [
+path('admin/', admin.site.urls),
+path('', include('pages.urls')),
+]
